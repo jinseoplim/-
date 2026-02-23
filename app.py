4d376d25969ada481519a70a1ab7c0685a1d6af6
jinseoplim <limjin2004@snu.ac.kr>
2026-02-23 23:19:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown("""
 
     /* 모든 버튼 규격 통일: 이름이 있든 없든 무조건 똑같은 직사각형 */
     .stButton > button {
-        width: 100% !important;
+        width: 200% !important;
         height: 45px !important; 
         min-height: 45px !important;
         max-height: 45px !important;
@@ -85,7 +85,7 @@ st.write("")
 
 # 5. 좌석 배치 (도면 일치 로직)
 for r in range(6):
-    cols = st.columns([1,1,1,1,1,1, 0.5, 1,1,1,1,1,1])
+    cols = st.columns([1,1,1,1,1,1, 0.8, 1,1,1,1,1,1])
     for c in range(6):
         if r == 0:
             l_idx = str(c + 1)
